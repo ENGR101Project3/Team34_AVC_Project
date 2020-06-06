5f5c31568c3fb37e21e8e58e1794e498cd5f9adb
Sam Dunnachie <39843254+FinniKey@users.noreply.github.com>
2020-06-06 20:59:14 +1200

    Completed Core Code
    The robot scans along the middlemost row of the image. If it detects a white pixel, it adds wherever it found it to a variable. It also stores how many white pixels it has come across so far.
    It gets the average position of the white pixel. From that, it calculates the distance between the average area of white pixels and the centre of the image. If that number is negative, it means that the white pixels were found in an area to the right of the centre. If that number is positive, it means that the white pixels were found in an area to the left of the centre. If the number is 0, then the area where the white pixels were found in the centre.
    Depending on whether the number is negative or positive, it makes one wheel slightly faster than the other wheel. This process repeats every frame.
    If the speed is changed, the robot may not be able to make sharp corners. We found 13 was a decent value to have one wheel, while the other stays at 10.
    
    The end result is a slight swaying motion as the robot always attempts to get the line in the exact centre. It still completes the maze, it's just a bit drunk.

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -4,45 +4,82 @@ int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 34.0;
-    double vRight = 30.0;
-    takePicture();
-    SavePPMFile("i0.ppm",cameraView);
+    double vLeft = 10.0;
+    double vRight = 10.0;
+    
+    int photoWidth = 150;
+	int photoHeight = 100;
+	
     while(1){
-      setMotors(vLeft,vRight);   
-      std::cout << "Bruh" << std::endl;
-      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-      usleep(1000);
-  } //while
+		
+		//take picture
+		takePicture();
+		SavePPMFile("i0.ppm",cameraView);
+		
+		//init var storing the total amount of white pixels we find - totalWhitePixels
+		int totalWhitePixels = 0;
+		//init var storing the index of all the white pixels we find - totalWhiteIndexes
+		int totalWhiteIndexes = 0;
+		
+		//for loop init y value, to make it in the centre, while the x value is the one that changes, so it'll scan along the centre row. 
+		for(int curCol = 0; curCol < photoWidth; curCol++) {
+			//get each pixel in the centre horizontal row. Use the getPixel to get RGB vals 
+			int curPixRed = get_pixel(cameraView, photoHeight / 2, curCol, 0);
+			int curPixGreen = get_pixel(cameraView, photoHeight / 2, curCol, 1);
+			int curPixBlue = get_pixel(cameraView, photoHeight / 2, curCol, 2);
+			
+			
+			//If all those are above 250 (play around with that one)
+			if(curPixRed > 250 && curPixGreen > 250 && curPixBlue > 250) {
+				std::cout << "curWhitePix: " << curCol << std::endl;
+				//add the index of whatever white is at to that var its being stored in
+				totalWhiteIndexes += curCol;
+				std::cout << "Total White Index: " << totalWhiteIndexes << std::endl;
+				//add one to a variable that counts the amount of ones (var++)
+				totalWhitePixels++;
+				std::cout << "Total White: " << totalWhitePixels << std::endl;
+			} //close if
+		} //close for
+		//calc the avg amount of 1's based on the indexes (indexes/1's)
+		int whitePositionAvg = (int) totalWhiteIndexes / totalWhitePixels;
+		//EXAMPLE: so, you've got three 1's there, so add each of where you saw each 1 (the index of the 1), and divide by the amount of 1's you saw. This value needs to be the new centre.
+		
+		//Calculate the distance between the average we just worked out and the centre pixel. If the avg is 80, but the centre is 75, then we need to move 5 pixels.
+		int distanceFromCentre = (int) (photoWidth/2) - whitePositionAvg;
+	
+		//Change the speed of the wheels based on that difference
+		//The robot would then turn based on whatever number it is.
 
+		//Is the distance from the centre greater than 0?
+		if(distanceFromCentre > 0) {
+			//Yes, which means the robot needs to turn left.
+			std::cout << "Left" << std::endl;
+			vLeft = 10.0;
+			vRight = 13.0;
+		}
+		//Is the distance from the centre less than 0?
+		else if(distanceFromCentre < 0) {
+			//Yes, which means the robot needs to turn right.
+			std::cout << "Right" << std::endl;
+			vLeft = 13.0;
+			vRight = 10.0;
+		}
+		//Does the distance from the centre equal 0?
+		else if(distanceFromCentre == 0) {
+			//Yes, which means the robot doesn't need to do anything except go straight.
+			std::cout << "Forward" << std::endl;
+			vLeft = 10.0;
+			vRight = 10.0;
+		}
+		setMotors(vLeft, vRight);
+		//setMotors(0,0);
+		std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+		std::cout<<std::endl;
+		//usleep(1000);
+  } //while
 } // main
 
-//take picture
-//init var storing the total amount of white pixels we fine - totalWhitePixels
-//init var storing the index of all the white pixels we find - totalWhiteIndexes
-//for loop init y value, to make it in the centre, while the x value is the one that changes, so it'll scan along the centre row. 
-//get each pixel in the centre horizontal row. Use the getPixel to get RGB vals 
-//If all those are above 250 (play around with that one)
-	//add the index of whatever white is at to that var its being stored in
-	//add one to a variable that counts the amount of ones (var++)
-//close for loop
-//calc the amount of 1's for the total line
-//calc the avg amount of 1's based on the indexes (indexes/1's)
-	//EXAMPLE: so, you've got three 1's there, so add each of where you saw each 1 (the index of the 1), and divide by the amount of 1's you saw. This value needs to be the new centre.
-//Calculate the distance between the average we just worked out and the centre pixel. If the avg is 80, but the centre is 75, then we need to move 5 pixels.
 
-//Move the robot
-
-//Play around with this one, just to see how it works.
-
-//Change the speed of the wheels based on that difference
-	//We could divide the difference by the absolute difference. This would give us 1 or -1. Or 0, I guess. 
-	//The robot would then turn based on whatever number it is.
-	
-	//Use the equations Aruthur gave us 
-	//Vright = v_go + dv
-	//Vleft = v_go - dv
-	//dv = the difference btwn L&R
 
 
 

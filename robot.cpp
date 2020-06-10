6de1ea96c475228a3afb97c50abdeb79546942de
Emily Rhodes-Robinson <erhodesrobinson@gmail.com>
2020-06-11 01:23:47 +1200

    Updated Documentation & Code Efficiency
    1) Comments have been cleaned up and updated to suit the current code.
    2) Efficiency has been added by creating a new variable 'vDefault' which is the set speed of the robot. This means if we want to change the speed of the robot, we can do so by simply changing one value rather than multiple. The change in L and R wheel velocities are also now scaled in accordance to the default velocity (however still mostly accurate to default velocities around 10-30 range).

--- robot.cpp
+++ robot.cpp
@@ -1,89 +1,94 @@
-#include "robot.hpp"
+ #include "robot.hpp"
 
-double vLeft = 10;
-double vRight = 10;
+ //Initialising variables 
+ double vDefault = 10;		//default L & R velocities of
+ double vLeft = vDefault;
+ double vRight = vDefault;
 
-int photoWidth = 150;
-int photoHeight = 100;
+ int photoWidth = 150;
+ int photoHeight = 100;
 
-void adjustSpeed(double vL, double vR) {
+ //Adjusts the left and right velocities
+ void adjustSpeed(double vL, double vR) {
 	vLeft = vL;
 	vRight = vR;
-}
+ }
 
-int main(){
+ int main(){
 	if (initClientRobot() !=0){
-		std::cout<<" Error initializing robot"<<std::endl;
-	}
-       
-    while(1){
-		//take picture
-		takePicture();
-		SavePPMFile("i0.ppm",cameraView);
-		
-		int totalWhitePixels = 0; //Stores the total amount of white pixels we find
-		int totalWhiteIndexes = 0; //Stores the index of all the white pixels we find
-		
-		//for loop init y value, to make it in the centre, while the x value is the one that changes, so it'll scan along the centre row. 
-		for(int curCol = 0; curCol < photoWidth; curCol++) {
-			//get each pixel in the centre horizontal row. Use the getPixel to get RGB vals 
+ 		std::cout<<" Error initializing robot"<<std::endl;
+ 	}
+	
+	while(1){	//loops forever
+		//Takes picture
+ 		takePicture();
+ 		SavePPMFile("i0.ppm",cameraView);
+
+		//Stores white pixel information	 (used to calc centre white pixel)
+ 		int totalWhitePixels = 0; //Stores total amount of white pixels
+ 		int totalWhiteIndexes = 0; //Stores total of indexes of each white pixels
+
+ 		//Scans pixels in bottom horizontal row 	(x val increments, y val is constant)
+ 		for(int curCol = 0; curCol < photoWidth; curCol++) {
+			
+ 			//Uses getPixel method to get RGB values of pixel 
 			int curPixRed = get_pixel(cameraView, photoHeight/1.01, curCol, 0);
-			int curPixGreen = get_pixel(cameraView, photoHeight/1.01, curCol, 1);
-			int curPixBlue = get_pixel(cameraView, photoHeight/1.01, curCol, 2);
+ 			int curPixGreen = get_pixel(cameraView, photoHeight/1.01, curCol, 1);
+ 			int curPixBlue = get_pixel(cameraView, photoHeight/1.01, curCol, 2);
+
+ 			//Calculates quantity & location of white pixels
+ 			if(curPixRed > 250 && curPixGreen > 250 && curPixBlue > 250) { //if RGB vals are all more than 250, then pixel is considered white
+ 				std::cout << "curWhitePix: " << curCol << std::endl;
+ 				
+ 				totalWhiteIndexes += curCol; //adds index of white pixel to total of indexes
+ 				std::cout << "Total White Index: " << totalWhiteIndexes << std::endl;
+ 				
+ 				totalWhitePixels++; //increments total white pixels by one
+ 				std::cout << "Total White: " << totalWhitePixels << std::endl;
+ 			} //close if
+ 		} //close for
+
+
+		//Calculates direction change of robot (by L/R wheel velocities)
+ 		if (totalWhitePixels != 0) { //checks if there is white line to follow
 			
-			//If all those are above 250 (play around with that one)
-			if(curPixRed > 250 && curPixGreen > 250 && curPixBlue > 250) {
-				std::cout << "curWhitePix: " << curCol << std::endl;
-				//add the index of whatever white is at to that var its being stored in
-				totalWhiteIndexes += curCol;
-				std::cout << "Total White Index: " << totalWhiteIndexes << std::endl;
-				//add one to a variable that counts the amount of ones (var++)
-				totalWhitePixels++;
-				std::cout << "Total White: " << totalWhitePixels << std::endl;
-			} //close if
-		} //close for
-		
-		if (totalWhitePixels != 0) {
-			//calc the avg amount of 1's based on the indexes (indexes/1's)
+ 			//Calculates the avg index of white pixels - this avg index needs to be the new centre
 			int whitePositionAvg = (int) totalWhiteIndexes / totalWhitePixels;
-			//EXAMPLE: so, you've got three 1's there, so add each of where you saw each 1 (the index of the 1), and divide by the amount of 1's you saw. This value needs to be the new centre.
-			
-			//Calculate the distance between the average we just worked out and the centre pixel. If the avg is 80, but the centre is 75, then we need to move 5 pixels.
-			int distanceFromCentre = (int) (photoWidth/2) - whitePositionAvg;
-		
-			//Change the speed of the wheels based on that difference
-			//The robot would then turn based on whatever number it is.
+ 			
+ 			//Calculates distance between avg white pixel and centre pixel
+ 			int distanceFromCentre = (int) (photoWidth/2) - whitePositionAvg;
 
-			//Is the distance from the centre greater than 0?
-			if(distanceFromCentre > 0) {
-				//Yes, which means the robot needs to turn left.
-				std::cout << "Left" << std::endl;
-				adjustSpeed(10.0, 10.0+(0.5*distanceFromCentre));
-			}
-			//Is the distance from the centre less than 0?
-			else if(distanceFromCentre < 0) {
-				//Yes, which means the robot needs to turn right.
-				std::cout << "Right" << std::endl;
-				adjustSpeed(10.0-(0.5*distanceFromCentre), 10.0);
-			}
-			
-			//Does the distance from the centre equal 0?
-			else if(distanceFromCentre == 0) {
-				//Yes, which means the robot doesn't need to do anything except go straight.
-				std::cout << "Forward" << std::endl;
-				adjustSpeed(10.0, 10.0);
-			} 
-			
-			setMotors(vLeft, vRight);
-			//setMotors(0,0);
-			std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-			std::cout<<std::endl;
-			//usleep(1000);
-		} else {
-			std::cout<<"WhiteLine Gone";
-			break;
-		}
+ 			
+ 			//Changes the speed of the wheels based on the difference & following conditions
 
-  } //while
-} // main
+ 			//Turns robot left if robot is too far right
+ 			if(distanceFromCentre > 0) { 	//positive difference means too far right
+ 				std::cout << "Left" << std::endl;
+ 				adjustSpeed(vDefault, vDefault+(vDefault*0.05*distanceFromCentre)); //vLeft < vRight
+ 			}
+ 			//Turns robot right if robot is too far left
+ 			else if(distanceFromCentre < 0) {  	 //negative difference means too far left
+ 				std::cout << "Right" << std::endl;
+ 				adjustSpeed(vDefault-(vDefault*0.05*distanceFromCentre), vDefault); //vLeft > vRight
+ 			}
+ 			//Makes robot go straight if robot is centered
+ 			else if(distanceFromCentre == 0) {	  //no difference means no error
+ 				std::cout << "Forward" << std::endl;
+ 				adjustSpeed(vDefault, vDefault); //vLeft = vRight
+ 			} //closes velocity change (if)
 
+			//Sets L and R motors to adjusted velocities
+ 			setMotors(vLeft, vRight);
+ 			//setMotors(0,0);
+ 			std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+ 			std::cout<<std::endl;
+ 			//usleep(1000);
+ 			
+ 		} else { //no white line detected
+ 			std::cout<<"No white line detected.";
+ 			break;
+ 		} //closes if
+
+   } //closes while
+   
+ } // closes main
